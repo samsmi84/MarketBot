7a1c7393179c91b5aa038da212cb9773e22fa992
Samuel Smith <63981393+samsmi84@users.noreply.github.com>
2020-11-06 14:36:21 +1300

    made a chat bot and added functions to it

---
+++ Script/Scripter.js
@@ -0,0 +1,12 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("001").style.display = "block";
+}
+
+function hideButton() {
+    document.getElementById("001").style.display = "none";
+}
